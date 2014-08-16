ab12fc603679740982843cbe564fd8f43fc9ae2a
Mark Grimes <mgrimes@cpan.org>
2014-08-16 14:01:46 +0000

    Import of MGRIMES/Log-Any-Adapter-Catalyst-1.00 from CPAN.
    gitpan-cpan-distribution: Log-Any-Adapter-Catalyst
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         MGRIMES/Log-Any-Adapter-Catalyst-1.00.tar.gz
    gitpan-cpan-author:       MGRIMES
    gitpan-cpan-maturity:     released

--- lib/Log/Any/Adapter/Catalyst.pm
+++ lib/Log/Any/Adapter/Catalyst.pm
@@ -1,11 +1,14 @@
 package Log::Any::Adapter::Catalyst;
-use Log::Any::Adapter::Util qw(make_method);
+
+# ABSTRACT: Enable error and status logging in Catalyst Models via Log::Any
+
 use strict;
 use warnings;
 use Carp;
+use Log::Any::Adapter::Util qw(make_method);
 use base qw(Log::Any::Adapter::Base);
 
-our $VERSION = '0.11';
+our $VERSION = '1.00';
 
 sub init {
     my ($self) = @_;
@@ -40,6 +43,10 @@ __END__
 =head1 NAME
 
 Log::Any::Adapter::Catalyst - Enable error and status logging in Catalyst Models via Log::Any
+
+=head1 VERSION
+
+version 1.00
 
 =head1 SYNOPSIS
 
@@ -82,13 +89,13 @@ L<Catalyst|Catalyst>, L<Catalyst::Log|Catalyst::Log>
 
 =head1 AUTHOR
 
-Mark Grimes
+Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>
 
-=head1 COPYRIGHT & LICENSE
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 Mark Grimes, all rights reserved.
+This software is copyright (c) 2014 by Mark Grimes, E<lt>mgrimes@cpan.orgE<gt>.
 
-This program is free software; you can redistribute it and/or modify it under
-the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
